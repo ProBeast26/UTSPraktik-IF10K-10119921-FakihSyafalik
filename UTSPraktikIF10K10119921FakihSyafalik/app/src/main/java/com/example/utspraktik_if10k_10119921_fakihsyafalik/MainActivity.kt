859eb21ca19c4a22618a782f9b50b77104678f3d
ProBeast26 <130539196+ProBeast26@users.noreply.github.com>
2023-05-30 21:00:05 +0700

    Add files via upload
    first commit

---
+++ UTSPraktikIF10K10119921FakihSyafalik/app/src/main/java/com/example/utspraktik_if10k_10119921_fakihsyafalik/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if10k_10119921_fakihsyafalik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
